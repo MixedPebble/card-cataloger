63ede08fcf5601198682c89a7a9354e988451bfa
mixedpebble <danielkjackson95@gmail.com>
2025-08-25 00:00:34 -0700

    created propper main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import card_capture
 load_dotenv()
 
 from mtgscan.text import MagicRecognition
@@ -7,9 +8,4 @@ from mtgscan.ocr.azure import Azure
 
 
 if __name__ == "__main__":
-    azure = Azure()
-    rec = MagicRecognition(file_all_cards="all_cards.txt", file_keywords="Keywords.json")
-    box_texts = azure.image_to_box_texts("https://assets-prd.ignimgs.com/2025/07/25/rarest-mtg-cards-1753454598010.jpg")
-    deck = rec.box_texts_to_deck(box_texts)
-    for card_name, count in deck:
-        print(card_name, count)
+    card_capture.run()
